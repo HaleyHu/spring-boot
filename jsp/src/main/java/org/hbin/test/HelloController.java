68d5438420d81befaaec8d7e43e9c799b6cd1e99
hu <haib.hu@haihangyun.com>
2017-06-13 11:29:57 +0800

    java & xml

--- jsp/src/main/java/org/hbin/test/HelloController.java
+++ jsp/src/main/java/org/hbin/test/HelloController.java
@@ -3,12 +3,16 @@ package org.hbin.test;
 import java.util.Date;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
+	private Logger log = LoggerFactory.getLogger(HelloController.class);
+	
 	@Value("${application.msg:XX}")
 	private String msg;
 
@@ -16,14 +20,15 @@ public class HelloController {
 	public String hello(Map<String, Object> map) {
 		Date now = new Date();
 		map.put("time", now);
-		System.out.println("[HelloController hello()] time: " + now);
+		log.debug("time: " + now);
+		log.info("time: " + now);
 		return "hello";
 	}
 
 	@RequestMapping("/hi")
 	public String hi(Map<String, Object> map) {
 		map.put("msg", msg);
-		System.out.println("[HelloController hi()] msg: " + msg + ", time: " + new Date());
+		log.debug("msg: " + msg + ", time: " + new Date());
 
 		return "hello";
 	}
